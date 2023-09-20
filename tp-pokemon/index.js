1114504743b71162bbbca70706da5eb8637cbd79
Nassim Sakhri <tails01@hotmail.fr>
2023-09-20 16:45:12 +0200

    changement code tp-pokemon

--- tp-pokemon/index.js
+++ tp-pokemon/index.js
@@ -1,10 +1,10 @@
 const url = "https://pokeapi.co/api/v2/pokemon/"
-
-let nb1 = Math.floor(Math.random() * 493) + 1;
+let max = 493
+let nb1 = Math.floor(Math.random() * max) + 1;
 let nb1string = url + nb1
 
 console.log(nb1string);
-nb1 = Math.floor(Math.random() * 493) + 1;
+nb1 = Math.floor(Math.random() * max) + 1;
 
 // 1. Faites une requête API pour obtenir les données
 fetch(nb1string)
@@ -92,5 +92,106 @@ function changePokemon() {
 
 }
 
+function precedentPokemon() {
+    if (nb1 > 1) {
+        nb1string = url + (nb1-1)
+
+
+        fetch(nb1string)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('La réponse de l\'API n\'est pas OK');
+                }
+                return response.json();
+            })
+            .then(data => {
+
+                jsonData = JSON.stringify(data);
+
+
+                localStorage.setItem('pokemon1', jsonData);
+
+                console.log('Données de l\'API enregistrées dans le localStorage.');
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des données de l\'API :', error);
+            });
+
+
+        jsonData = localStorage.getItem('pokemon1');
+
+        poke = document.getElementById("poke")
+
+        data = JSON.parse(jsonData);
+
+        sprite = data.sprites.front_default;
+        Name = data.name;
+        height = data.height;
+        weight = data.weight;
+
+        poke.src = sprite;
+        namepokemon.innerHTML = Name;
+        heighpokemon.innerHTML = height;
+        weighpokemon.innerHTML = weight;
+    }
+    else{
+        alert("Ce ne sera pas possible d'aller au dessous")
+    }
+}
+
+function suivantPokemon() {
+    if (nb1 < max) {
+        nb1string = url + (nb1+1)
+
+
+        fetch(nb1string)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('La réponse de l\'API n\'est pas OK');
+                }
+                return response.json();
+            })
+            .then(data => {
+
+                jsonData = JSON.stringify(data);
+
+
+                localStorage.setItem('pokemon1', jsonData);
+
+                console.log('Données de l\'API enregistrées dans le localStorage.');
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des données de l\'API :', error);
+            });
+
+
+        jsonData = localStorage.getItem('pokemon1');
+
+        poke = document.getElementById("poke")
+
+        data = JSON.parse(jsonData);
+
+        sprite = data.sprites.front_default;
+        Name = data.name;
+        height = data.height;
+        weight = data.weight;
+
+        poke.src = sprite;
+        namepokemon.innerHTML = Name;
+        heighpokemon.innerHTML = height;
+        weighpokemon.innerHTML = weight;
+    }
+    else{
+        alert("Ce ne sera pas possible d'aller au dela")
+    }
+}
+
+
 const change = document.getElementById("change")
 change.addEventListener("click", changePokemon)
+
+let precedent = document.getElementById("precedent")
+precedent.addEventListener("click", precedentPokemon)
+
+let suivant = document.getElementById("suivant")
+suivant.addEventListener("click", suivantPokemon)
